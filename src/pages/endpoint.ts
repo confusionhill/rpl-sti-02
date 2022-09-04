d56d0442295f49563786d188b4b47861fcdbdbb6
farhan <18220015@std.stei.itb.ac.id>
2022-09-04 21:37:27 +0700

    added endpoint

---
+++ src/pages/endpoint.ts
@@ -0,0 +1,12 @@
+export const appEndPoint = [
+    {
+        label: "Daftar Order",
+        url: "/daftar-order",
+        icon: "",
+    },
+    {
+        label: "Daftar Order",
+        url: "/daftar-order",
+        icon: "",
+    },
+]
